8d6b053fb6469dae5b97c84737232c6b8ff7e85d
Alex <96dalex@gmail.com>
2015-05-27 16:38:04 -0600

    made a unit class, changed some things to use the class

--- game.js
+++ game.js
@@ -1,33 +1,66 @@
-var squaresOnBoard = [];
-var player = {
-  squares: [[2, 2]],
-  head: [2, 2],
+var board = { squares: [] };
+
+function Unit(options, loc) {
+  this.maxMoves = options.maxMoves;
+  this.maxLength = options.maxLength;
+  this.movesMade = 0;
+  this.head = loc;
+  this.squares = [loc];
+  this.attackMode = false;
+  this.attacks = options.attacks;
+  this.moveOver = false;
+}
+
+Unit.prototype.movesRemaining = function() {
+  return this.maxMoves - this.movesMade;
+};
+
+Unit.prototype.currentAttack = function() {
+  return this._currentAttack || this.attacks[0];
+};
+
+Unit.prototype.attack = function(enemy) {
+  this.attackMode = false;
+  this.moveOver = true;
+  enemy.removeSquares(this.currentAttack().damage);
+};
+
+Unit.prototype.removeSquares = function(amount) {
+  this.squares = this.squares.slice(0, -amount);
+};
+
+Unit.prototype.restartTurn = function() {
+  this.movesMade = 0;
+  this.attackMode = false;
+  this.moveOver = false;
+};
+
+Unit.prototype.makeButtonsForAttacks = function() {
+  unit = this;
+  return this.attacks.map(function(attack) {
+    return {
+      press: function() {
+        unit.attackMode = true;
+        unit._currentAttack = attack;
+      },
+      text: attack.name
+    };
+  });
+};
+
+
+var player = new Unit({
   maxLength: 3,
   maxMoves: 4,
-  movesMade: 0,
-  movesRemaining: function() {
-    return this.maxMoves - this.movesMade;
-  },
   attacks: [
     { name: 'attack1', range: 2, damage: 2 },
     { name: 'attack2', range: 1, damage: 3 }
-  ],
-  currentAttack: function() {
-    return this._currentAttack || this.attacks[0];
-  }
-};
+  ]
+}, [2, 2]);
 
 var attackMode = false;
 
-var buttons = player.attacks.map(function(attack) {
-  return {
-    press: function() {
-      attackMode = true;
-      player._currentAttack = attack;
-    },
-    text: attack.name
-  };
-});
+var buttons = player.makeButtonsForAttacks();
 
 var enemy = {
   squares: [[3, 4], [4, 4], [5, 4]],
@@ -74,7 +107,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
     for (var j = 0; j < yMany; j++) {
       var x = space + i*(size + space);
       var y = space + j*(size + space);
-      squaresOnBoard.push({ x: x + offset, y: y, size: size, loc: [i, j] });
+      board.squares.push({ x: x + offset, y: y, size: size, loc: [i, j] });
     }
   }
   drawOnCanvas(ctx);
@@ -84,8 +117,8 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
     var clickedSquare = null;
     var clickedButton = null;
-    for (var i = 0; i < squaresOnBoard.length; i++) {
-      var square = squaresOnBoard[i];
+    for (var i = 0; i < board.squares.length; i++) {
+      var square = board.squares[i];
       if(square.x < x && square.x + square.size > x && square.y < y && square.y + square.size > y) {
         clickedSquare = square;
         break;
@@ -99,15 +132,15 @@ document.addEventListener("DOMContentLoaded", function(event) {
         }
       }
     }
-    if(clickedSquare && attackMode && squareDist(player.head, clickedSquare.loc) <= player.currentAttack().range && isInArray(enemy.squares, clickedSquare.loc)) {
+    if(clickedSquare && player.attackMode && squareDist(player.head, clickedSquare.loc) <= player.currentAttack().range && isInArray(enemy.squares, clickedSquare.loc)) {
       enemy.squares = enemy.squares.slice(0, -player.currentAttack().damage);
-    } else if(!attackMode && clickedSquare && player.movesRemaining() > 0 && squareNextTo(clickedSquare.loc, player.head)) {
+    } else if(!player.attackMode && clickedSquare && player.movesRemaining() > 0 && squareNextTo(clickedSquare.loc, player.head)) {
       player.head = clickedSquare.loc;
       player.squares.unshift(clickedSquare.loc);
       player.squares = player.squares.slice(0, player.maxLength);
       player.movesMade++;
       if(player.movesRemaining() === 0) {
-        attackMode = true;
+        player.attackMode = true;
       }
     } else if(clickedButton) {
       clickedButton.press();
@@ -123,20 +156,20 @@ function squareNextTo(a, b) {
 
 function drawOnCanvas(ctx) {
   clearCanvas(ctx);
-  squaresOnBoard.forEach(function(square) {
+  board.squares.forEach(function(square) {
     if(arrayEqual(player.head, square.loc)) {
       ctx.fillStyle = color3;
     } else if(isInArray(player.squares, square.loc)) {
       ctx.fillStyle = color2;
     } else if(isInArray(enemy.squares, square.loc)) {
       ctx.fillStyle = color6;
-    } else if(!attackMode && squareDist(square.loc, player.head) <= player.movesRemaining()) {
+    } else if(!player.attackMode && squareDist(square.loc, player.head) <= player.movesRemaining()) {
       ctx.fillStyle = color4;
     } else {
       ctx.fillStyle = color1;
     }
     ctx.fillRect(square.x, square.y, square.size, square.size);
-    if(attackMode && squareDist(square.loc, player.head) <= player.currentAttack().range && !isInArray(player.squares, square.loc)) {
+    if(player.attackMode && squareDist(square.loc, player.head) <= player.currentAttack().range && !isInArray(player.squares, square.loc)) {
       ctx.fillStyle = color5;
       ctx.textAlign = 'center';
       ctx.font = '' + size + 'px monospace';
